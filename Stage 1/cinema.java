9695d32a7603560abfc5daa91988189d7dc4b24d
Ben Basty <benbasty@yahoo.fr>
2021-10-03 21:49:18 +0800

    Create cinema.java

---
+++ Stage 1/cinema.java
@@ -0,0 +1,17 @@
+package cinema;
+
+public class Cinema {
+
+    public static void main(String[] args) {
+        // Write your code here
+        System.out.println("Cinema:");
+        System.out.println(" " + " " + 1 + " " + 2 + " " + 3 + " " + 4 + " " + 5 + " " + 6 + " " + 7 + " " + 8);
+        for (int i = 1; i < 8; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < 8; j++) {
+                System.out.print("S " );
+            }
+            System.out.println();
+        }
+    }
+}
